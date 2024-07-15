894c84203b9074bb71c7eff0694b88fdd9718172
ahu-at-speer <alan.hu@speer.io>
2024-07-15 11:59:07 -0400

    First Commit

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'quoting', label: 'Quoting Tool' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
